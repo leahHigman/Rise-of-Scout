dbd76bd1ce800ea7a721981d433d27dee4092009
Leah <higmangirardl@my.lanecc.edu>
2026-01-05 15:51:15 -0800

    added inputs

---
+++ rise-of-scout/player/scout.gd
@@ -0,0 +1,13 @@
+class_name Player extends CharacterBody2D
+
+@export var speed = 500
+@export var gravity = 4000
+@export var jumpImpulse = 1800
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

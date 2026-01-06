ddc6a5da0fe8ddde08df04a7ec90cea16a81d219
Leah <higmangirardl@my.lanecc.edu>
2026-01-05 16:25:21 -0800

    functioning state machine (I stole this)
    I copied someone elses code to help me understand state machines

--- rise-of-scout/player/scout.gd
+++ rise-of-scout/player/scout.gd
@@ -1,8 +1,8 @@
 class_name Player extends CharacterBody2D
 
-@export var speed = 500
-@export var gravity = 4000
-@export var jumpImpulse = 1800
+@export var speed := 500
+@export var gravity := 4000
+@export var jump_impulse := 1800
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
